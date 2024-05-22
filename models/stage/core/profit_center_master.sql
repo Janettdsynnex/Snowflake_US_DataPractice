be6e9bfcec78b47f2c5900de26e4fd13e0118a7f
Janet Rajan <T628526Q@techdata.com>
2024-05-22 23:38:04 +0000

    Merged PR 81703: CDP Tables updates

--- models/stage/core/profit_center_master.sql
+++ models/stage/core/profit_center_master.sql
@@ -64,3 +64,32 @@ select
     , NULL as TM1_HRCHY_L4_TEXT
     , SYSDATE() as UPDATE_DATE_UTC
 from {{source('us_cdp_cis_us','DIM_PUB_BIZ_SEGMENT_HIERARCHY_US')}}
+-- ca
+union all
+select
+    md5(concat('CIS_CA', to_char(VPL_NO))) as PROFIT_CENTER_MASTER_KEY
+    , to_char(VPL_NO) as PROFIT_CENTER
+    , NULL as CONTROLLING_AREA
+    , 'CIS_CA' as SOURSYSTEM
+    , NULL as FUNCTIONAL_AREA
+    , VPL_DESC as VENDOR_DIFFERENT
+    , UNIVERSAL_VEND_NAME as GLBL_MFR
+    , NULL as SBU_HRCHY_L0
+    , SOLUTION_CODE as SBU_HRCHY_L1
+    , SOLUTION_NAME as SBU_HRCHY_L1_TEXT
+    , to_char(LEVEL_1_SEG_ID) as SBU_HRCHY_L2
+    , LEVEL_1_NAME as SBU_HRCHY_L2_TEXT
+    , to_char(LEVEL_2_SEG_ID) as SBU_HRCHY_L3
+    , LEVEL_2_NAME as SBU_HRCHY_L3_TEXT
+    , to_char(LEVEL_3_SEG_ID) as SBU_HRCHY_L4
+    , LEVEL_3_NAME as SBU_HRCHY_L4_TXT
+    , NULL as TM1_HRCHY_L1
+    , NULL as TM1_HRCHY_L1_TEXT
+    , NULL as TM1_HRCHY_L2
+    , NULL as TM1_HRCHY_L2_TEXT
+    , NULL as TM1_HRCHY_L3
+    , NULL as TM1_HRCHY_L3_TEXT
+    , NULL as TM1_HRCHY_L4
+    , NULL as TM1_HRCHY_L4_TEXT
+    , SYSDATE() as UPDATE_DATE_UTC
+from {{source('ca_cdp_cis_ca','DIM_PUB_BIZ_SEGMENT_HIERARCHY_CA')}}
